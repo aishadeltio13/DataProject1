e00129e8804afef13db7dd84e2040077365d30ca
Miguel Ángel Navarro <miguelanav01@gmail.com>
2026-01-16 00:31:55 +0100

    DBT: implement staging model for London air quality data transformation
    - Configured 'sources.yml' with dynamic environment variables for database connectivity.
    - Created 'stg__london_qair.sql' model to parse 'raw_data' JSONB column into structured fields.
    - Applied data type casting (INT, FLOAT) and ensured global scalability using 'TIMESTAMPTZ' for measurement times.
    - Implemented 'NULLIF' logic to handle non-numeric API values ("-") and prevent casting errors.
    - Successfully extracted geographic coordinates (latitude/longitude) from nested JSON arrays.
    - Validated the transformation pipeline using Docker-based dbt execution (dbt run/show).

---
+++ DBT/target/run/air_project_london/models/staging/stg__london_qair.sql
@@ -0,0 +1,22 @@
+
+  create view "air_quality_db"."public"."stg__london_qair__dbt_tmp"
+    
+    
+  as (
+    SELECT
+    NULLIF(raw_data -> 'data' ->> 'aqi', '-'):: INT AS                        aqi_index,    -- real time air quality information
+    NULLIF(raw_data -> 'data' ->> 'idx', '-'):: INT AS                        station_id,
+    (raw_data -> 'data' -> 'city' ->> 'name'):: VARCHAR AS                    station_name,
+    NULLIF(raw_data -> 'data' -> 'city' -> 'geo' ->> 0, '-'):: FLOAT AS       latitude,
+    NULLIF(raw_data -> 'data' -> 'city' -> 'geo' ->> 1, '-'):: FLOAT AS       longitude,
+    (raw_data -> 'data' -> 'time' ->> 'iso'):: TIMESTAMPTZ AS                 api_measurement_time,
+    NULLIF(raw_data -> 'data' -> 'iaqi' -> 'pm25' ->> 'v', '-'):: FLOAT AS    pm25,
+    NULLIF(raw_data -> 'data' -> 'iaqi' -> 'pm10' ->> 'v', '-'):: FLOAT AS    pm10,
+    NULLIF(raw_data -> 'data' -> 'iaqi' -> 'no2' ->> 'v', '-'):: FLOAT AS     no2,
+    NULLIF(raw_data -> 'data' -> 'iaqi' -> 'o3' ->> 'v', '-'):: FLOAT AS      o3,
+    NULLIF(raw_data -> 'data' -> 'iaqi' -> 'so2' ->> 'v', '-'):: FLOAT AS     so2,    -- low priority (only for ports or industrial areas)
+    NULLIF(raw_data -> 'data' -> 'iaqi' -> 'co' ->> 'v', '-'):: FLOAT AS      co,     -- low priority (only for ports or industrial areas)
+    ingestion_time
+
+FROM "air_quality_db"."public"."london_raw_data"
+  );
