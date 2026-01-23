cda8e4cb00e3b2051c85005a7b11c24330db9d13
Aisha del Tio <adeltiodeprado@gmail.com>
2026-01-23 12:48:31 +0100

    Add area-level AQI statistics to historical ref model
    Introduces a new CTE 'area_calculations' to compute average, max, min, variance, standard deviation, and sample count of AQI values per area and pollutant from historical data.

--- DBT/models/intermediate/int__historical_ref.sql
+++ DBT/models/intermediate/int__historical_ref.sql
@@ -1,5 +1,5 @@
 with all_calculated_data as (
-    -- Reed from model with AQI calculated
+    -- Read from model with AQI calculated
     select * from {{ ref('int__london__aqi_calculations') }}
 ),
 
@@ -9,3 +9,19 @@ historical_only as (
     where data_origin = 'historical_data'
 ),
 
+area_calculations as (
+    select
+        area_code,     -- Use the cuadrants calculated in int__london__aqi_calculations
+        parameter,     -- Separate each pollutant
+        -- Statistical analysis
+        avg(aqi_value)      as avg_aqi_zone,
+        max(aqi_value)      as max_aqi_zone,
+        min(aqi_value)      as min_aqi_zone,
+        variance(aqi_value) as variance_aqi_zone,
+        stddev(aqi_value)   as stddev_aqi_zone,
+        count(*)            as total_samples
+    from historical_only
+    group by 1, 2
+)
+
+select * from area_calculations
